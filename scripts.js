7e45fc226471459f0035349170fd31df8b8f7a3c
Pranomvignesh <pranovignesh@gmail.com>
2020-06-13 13:21:36 +0530

    intial commit

---
+++ scripts.js
@@ -0,0 +1,34 @@
+require.config({
+    paths: {
+        vs: "./node_modules/monaco-editor/dev/vs"
+    }
+})
+
+require(["vs/editor/editor.main"], function(){
+    const editorDiv = document.querySelector('.editorDiv');
+    const jsString = `
+    const value = (function(){
+        return{
+            key1 : 'value',
+            key2 : 'value1'
+        }
+    }());
+    value1 = {
+        key1 : 'value',
+        key2 : 'value1'
+    };
+    var value2 = {
+        key1 : 'value',
+        key2 : 'value1'
+    }`
+    const editorOptions = {
+        language : 'javascript'
+    }
+    const compilerOptions = {
+        noImplicitUseStrict : true
+    }
+    const jsModel = monaco.editor.createModel(jsString,"javascript");
+    const jsContainer = monaco.editor.create(editorDiv, editorOptions);
+    jsContainer.setModel(jsModel);
+    // monaco.languages.typescript.javascriptDefaults.setCompilerOptions(compilerOptions)
+})
